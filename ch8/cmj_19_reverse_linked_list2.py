23faee99e008cd3beddd80dce73848537c8ce7f3
조미정 <ing06164@naver.com>
2024-03-24 17:40:36 +0900

    modify: chapter8_question19

--- ch8/cmj_19_reverse_linked_list2.py
+++ ch8/cmj_19_reverse_linked_list2.py
@@ -5,6 +5,25 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        # 예외 처리
+        if not head or left == right:
+            return head
+
+        root = start = ListNode(None)
+        root.next = head
+        # start, end 지정
+        for _ in range(left - 1):
+            start = start.next
+        end = start.next
+
+        # 반복하면서 노드 차례대로 뒤집기
+        for _ in range(right - left):  ## 이해가 안돼요
+            tmp, start.next, end.next = start.next, end.next, end.next.next
+            start.next.next = tmp
+
+        return root.next
+    
+    def reverseBetween2(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         # 구간 길이가 0인 경우 그냥 리턴
         if left == right:
             return head
