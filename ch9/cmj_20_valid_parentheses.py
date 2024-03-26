2e676bcd7b239ff9d9be56ae3a2b67c4cad7f442
조미정 <ing06164@naver.com>
2024-03-26 18:55:06 +0900

    modify: chapter9_question20

--- ch9/cmj_20_valid_parentheses.py
+++ ch9/cmj_20_valid_parentheses.py
@@ -2,28 +2,11 @@ class Solution:
     def isValid(self, s: str) -> bool:
         st = []
 
-        score = 0
         for i in range(len(s)):
             st.append(s[i])
 
-            # 현재 괄호가 짝이 같은 괄호인 경우 스택에서 pop
-            while len(st) >= 2 and st[-2] + st[-1] in ('()', '{}', '[]'):
+            while len(st) >= 2 and st[-2] + st[-1] in ('()', '{}', '[]'):         # 올바른 괄호인 경우 pop
                 st.pop()
                 st.pop()
-            
-            # 현재 괄호가 짝이 같지 않은 경우 score를 계산
-            # 현재 짝이 맞지 않더라도 최종적으로 socre가 0이고 스택이 비어있으면 올바른 괄호
-            if s[i] == '(':
-                score += 1
-            elif s[i] == '{':
-                score += 2
-            elif s[i] == '[':
-                score += 3
-            elif s[i] == ')':
-                score -= 1
-            elif s[i] == '}':
-                score -= 2
-            elif s[i] == ']':
-                score -= 3
 
-        return score == 0 and len(st) == 0
+        return len(st) == 0     # 스택 사이즈가 0이면 True
