e53e8a294ed413c8630ca3db44123039fa159dff
조미정 <ing06164@naver.com>
2024-05-02 16:44:33 +0900

    modify: chapter14 question43

--- ch14/cmj_43_diameter_of_binary_tree.py
+++ ch14/cmj_43_diameter_of_binary_tree.py
@@ -10,7 +10,7 @@ class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
         def dfs(node: TreeNode) -> int:
             if not node:
-                return -1
+                return -1         ### 존재하지 않는 노드의 리턴값 -1
 
             left = dfs(node.left)
             right = dfs(node.right)
@@ -23,3 +23,26 @@ class Solution:
 
         dfs(root)
         return self.longest
+    
+    def diameterOfBinaryTree2(self, root: TreeNode) -> int:
+        # 최대 지름을 저장할 변수
+        self.max_diameter = 0
+
+        def dfs(node):
+            if not node:
+                return 0          ###  존재하지 않는 노드의 리턴값 0, 즉 엣지수가 0임을 나타냄
+            
+            left = dfs(node.left)          
+            right = dfs(node.right)
+            
+            
+            # 최대 지름 업데이트
+            self.max_diameter = max(self.max_diameter, left + right)      ### 현재 노드의 엣지수 = left + right
+            
+            # 현재 노드의 깊이 반환
+            return max(left, right) + 1
+        
+        # 루트 노드에서 깊이 계산을 시작
+        dfs(root)
+        
+        return self.max_diameter
