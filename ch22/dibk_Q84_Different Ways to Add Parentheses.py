076839d81d076e9b61ac25ab1dbf37506c73143e
dibk100 <dkbk_100@naver.com>
2024-06-07 07:04:21 +0900

    modify:Q84

---
+++ ch22/dibk_Q84_Different Ways to Add Parentheses.py
@@ -0,0 +1,30 @@
+'''
+241. Different Ways to Add Parentheses
+https://leetcode.com/problems/different-ways-to-add-parentheses/description/
+
+[time] failed
+[문제] 
+[풀이방식] :
+- 이건 진짜 아니에요,, ㅏㅇ나ㅣ
+
+'''
+class Solution:
+    def diffWaysToCompute(self, input: str) -> List[int]:
+        def compute(left, right, op):
+            results = []
+            for l in left:
+                for r in right:
+                    results.append(eval(str(l) + op + str(r)))
+            return results
+
+        if input.isdigit():
+            return [int(input)]
+
+        results = []
+        for index, value in enumerate(input):
+            if value in "-+*":
+                left = self.diffWaysToCompute(input[:index])
+                right = self.diffWaysToCompute(input[index + 1:])
+
+                results.extend(compute(left, right, value))
+        return results
