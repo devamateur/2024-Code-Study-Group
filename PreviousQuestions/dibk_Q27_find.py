7e75908ff6818a89ebbcf87eda6210303dcfffcf
dibk100 <dibk_100@naver.com>
2024-09-02 19:42:38 +0900

    update

--- PreviousQuestions/dibk_Q27_find.py
+++ PreviousQuestions/dibk_Q27_find.py
@@ -43,3 +43,33 @@ class Solution :
     
 test = Solution()
 test.run()
+
+
+# bisect 라이브러리
+'''
+원소들이 **정렬된** 리스트에서 특정 원소를 찾을 때 효과적
+
+(1)  bisect_left(list, data): 리스트에 데이터를 삽입할 가장 왼쪽 인덱스를 찾는 함수(리스트 내 정렬 순서를 유지).
+
+(2)  bisect_right(list, data): 리스트에 데이터를 삽입할 가장 오른쪽 인덱스를 찾는 함수(리스트 내 정렬 순서를 유지).
+
+from bisect import bisect_left, bisect_right
+
+# [left_v, right_v] 범위 내에 있는 원소 개수 출력 함수
+def cnt_within_range (arr, left_v, right_v):
+    # 맨 좌측 인덱스
+    left_idx = bisect_left(arr, left_v)
+    # 맨 우측 인덱스
+    right_idx = bisect_right(arr, right_v)
+    return right_idx - left_idx
+
+# 리스트 생성
+arr = [5, 6, 7, 7, 7, 7, 8, 8, 9, 10]
+
+# 값이 9인 원소 개수 출력
+print(cnt_within_range(arr, 9, 9)) # 1
+# [4, 7] 범위 내에 있는 원소 개수 출력
+print(cnt_within_range(arr, 4, 7)) # 6
+
+
+'''
